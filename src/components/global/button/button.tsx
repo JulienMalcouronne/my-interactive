7cab8c8979a0cc5b909f523cdc4b836a50fb223c
julien malcouronne <malcouronnejulien@gmail.com>
2025-05-24 11:17:21 +0200

    feat: pass all attrs to button

--- src/components/global/button/button.tsx
+++ src/components/global/button/button.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+
+type ButtonProps = {
+  children: React.ReactNode;
+  bgColor: keyof typeof bgVariants;
+  onClick: () => void;
+} & React.HTMLAttributes<HTMLButtonElement>;
+
 const bgVariants: Record<string, string> = {
   blue: "bg-blue-600 hover:bg-blue-700",
   red: "bg-red-600 hover:bg-red-700",
@@ -10,15 +17,13 @@ export default function Button({
   children,
   bgColor,
   onClick,
-}: {
-  children: React.ReactNode;
-  bgColor: keyof typeof bgVariants;
-  onClick: () => void;
-}) {
+  ...rest
+}: ButtonProps) {
   return (
     <button
       className={`px-4 py-2 text-white rounded transition-colors cursor-pointer ${bgVariants[bgColor]}`}
       onClick={onClick}
+      {...rest}
     >
       {children}
     </button>
