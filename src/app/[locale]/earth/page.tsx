4766e9c7a55bd124cd2a3ab260512698b35e4192
julien malcouronne <malcouronnejulien@gmail.com>
2025-07-30 18:59:35 +0200

    fix: page reload issue

--- src/app/[locale]/earth/page.tsx
+++ src/app/[locale]/earth/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+'use client';
 
+import React from 'react';
 import dynamic from 'next/dynamic';
 
-const Earth = dynamic(() => import('@/components/earth/Earth'));
+const Earth = dynamic(() => import('@/components/earth/Earth'), {
+  ssr: false,
+});
 
 export default function Airplane() {
   return (
